87db69f15c8046d88c17ef9a54e826fa6c87adde
Toshiyuki SHIMONO <t.shimono@yamanashi.ac.jp>
2024-12-05 13:28:30 +0900

    or2abcd 一応出来た。a=d ∩ b=c を仮定した計算をする。

---
+++ or2abcd.pm
@@ -0,0 +1,25 @@
+package App::or2abcd ;  
+our $VERSION = '0.001' ; 
+our $DATE = '2024-12-05T10:00+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::orci2abcd - オッズ比とその95%信頼区間から、元の2x2クロス表の再現を試みる
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<or2abcd>'. 
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+or2abcd --help
+man or2abcd
+perldoc or2abcd
+
+=cut
+
+1 ;
